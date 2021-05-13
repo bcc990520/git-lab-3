fc119d643de55518253f311bf349ff665fa25e6d
Bikash Chauhan <bc990520@ohio.edu>
2021-05-12 22:26:22 -0400

    1st c++

---
+++ git-lab-program.cc
@@ -0,0 +1,15 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Bikash Chauhan>
+ *        Date: <MAY 12th>
+ * Description: Add Description
+ */
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+using namespace std;
+int main(int argc, char const *argv[])
+ {
+cout <<"Hello Git!!" << endl;
+return 0;
+}
